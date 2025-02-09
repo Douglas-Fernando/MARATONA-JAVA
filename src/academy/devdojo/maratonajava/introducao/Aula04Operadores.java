fd95f123def0eb043c84c34f709fd8f56e624d0e
Douglas Fernando <douglas021fernando_contato@outlook.com.br>
2025-02-09 11:48:44 -0400

    Operadores logicos v4

--- src/academy/devdojo/maratonajava/introducao/Aula04Operadores.java
+++ src/academy/devdojo/maratonajava/introducao/Aula04Operadores.java
@@ -42,5 +42,20 @@ public class Aula04Operadores {
         boolean isPlaystationCincoCompravel = valorTotalContaCorrente > valorPlaystation || valorTotalContaPoupanca > valorPlaystation;
         System.out.println("isPlaystationCincoCompravel "+isPlaystationCincoCompravel);
 
+        // = += -= *= /= %=
+        double bonus = 1800; // 1800
+        bonus += 1000; // 2800
+        bonus -= 1000; // 1800
+        bonus *= 2; // 3600
+        bonus /= 2; // 1800
+        bonus %= 2; // 0
+        System.out.println(bonus);
+
+        // ++ --
+        int contador = 0;
+        contador += 1; // contador = contador + 1
+        contador++; // soma de 1 e 1
+        contador--; // subtrai de 1 e 1
+        System.out.println(contador);
     }
 }
