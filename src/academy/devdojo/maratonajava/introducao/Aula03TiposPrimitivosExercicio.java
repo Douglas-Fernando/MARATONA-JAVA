70a0d5833d053081103d207274ce80a965fef049
Douglas Fernando <douglas021fernando_contato@outlook.com.br>
2025-02-08 21:46:06 -0400

    Primeiro exercicio da maratona do devdojo v1

---
+++ src/academy/devdojo/maratonajava/introducao/Aula03TiposPrimitivosExercicio.java
@@ -0,0 +1,17 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula03TiposPrimitivosExercicio {
+    public static void main(String[] args) {
+
+        /*
+        Eu <nome>, morando no endereço <endereço>, confirmo que recebi o salário de <salario>, na data <data>.
+         */
+        String nome = "Douglas Fernando ";
+        String endereco = "Rua boa vista, 1563 - bairro valparaiso";
+        double salario = 2690.03;
+        String data = "08/02/2025";
+        System.out.println("Eu " + nome + "morando no endereço " + endereco + ", confirmo que recebi o salário de " +
+                salario + ", na data " + data);
+
+    }
+}
