6cd34487e3746bf6e4ccb581d13ed1844a204d82
Douglas Fernando <douglas021fernando_contato@outlook.com.br>
2025-02-10 00:11:38 -0400

    Estruturas Condicionais v2

--- src/academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais.java
+++ src/academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais.java
@@ -2,8 +2,15 @@ package academy.devdojo.maratonajava.introducao;
 
 public class Aula05EstruturasCondicionais {
     public static void main(String[] args) {
-        if(true){
-            System.out.println("Dentro do if");
+        int idade = 16;
+        boolean isAutorizadoBebida = idade >= 18;
+        if(isAutorizadoBebida){
+            System.out.println("Autorizado");
         }
+        // !
+        if (!isAutorizadoBebida){
+            System.out.println("Não Autorizado");
+        }
+        System.out.println("Fora do if");
     }
 }
