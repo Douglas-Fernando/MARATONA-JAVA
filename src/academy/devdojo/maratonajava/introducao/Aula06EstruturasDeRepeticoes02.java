25652f61967ab7d2367e5bb8cf91ab216384ffb1
Douglas Fernando <douglas021fernando_contato@outlook.com.br>
2025-02-16 12:08:12 -0400

    Estruturas de repetição - exercicio v1

---
+++ src/academy/devdojo/maratonajava/introducao/Aula06EstruturasDeRepeticoes02.java
@@ -0,0 +1,12 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula06EstruturasDeRepeticoes02 {
+    public static void main(String[] args) {
+        // imprima todas os número pares de 0 até 1000000
+
+        for (int i = 0;i <= 100; i += 2){
+            System.out.println(i);
+
+        }
+    }
+}
