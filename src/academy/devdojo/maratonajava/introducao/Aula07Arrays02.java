27a7d3ede32d88a3bd9a0e9adefb249a42869476
Douglas Fernando <douglas021fernando_contato@outlook.com.br>
2025-02-19 21:56:20 -0400

    Arrays parte 2 v2

--- src/academy/devdojo/maratonajava/introducao/Aula07Arrays02.java
+++ src/academy/devdojo/maratonajava/introducao/Aula07Arrays02.java
@@ -3,13 +3,18 @@ package academy.devdojo.maratonajava.introducao;
 public class Aula07Arrays02 {
     public static void main(String[] args) {
         // byte, short, int, long, float e double o valor é 0
-        // char '\u000'
+        // char ''
         // boolean false
         // string null
 
-        int[] nomes = new int[3];
-        System.out.println(nomes[0]);
-        System.out.println(nomes[1]);
-        System.out.println(nomes[2]);
+        String[] nomes = new String[4];
+        nomes[0] = "Douglas";
+        nomes[1] = "Fernando";
+        nomes[2] = "Amaral";
+        nomes[3] = "Souza";
+
+        for (int i = 0; i < nomes.length; i++) {
+            System.out.println(nomes[i]);
+        }
     }
 }
