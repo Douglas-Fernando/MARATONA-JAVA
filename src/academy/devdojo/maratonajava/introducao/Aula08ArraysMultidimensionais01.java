1166998b11aaaa10a1c2612cd9a7ab8a4fcabee0
Douglas Fernando <douglas021fernando_contato@outlook.com.br>
2025-03-02 07:34:56 -0400

    Arrays Multidimensionais v2

--- src/academy/devdojo/maratonajava/introducao/Aula08ArraysMultidimensionais01.java
+++ src/academy/devdojo/maratonajava/introducao/Aula08ArraysMultidimensionais01.java
@@ -4,11 +4,11 @@ public class Aula08ArraysMultidimensionais01 {
     public static void main(String[] args) {
         int[][] dias = new int[3][3]; // sintaxe correta
         dias[0][0] = 31;
-        dias[0][1] = 31;
+        dias[0][1] = 28;
         dias[0][2] = 31;
 
         dias[1][0] = 31;
-        dias[1][1] = 31;
+        dias[1][1] = 28;
         dias[1][2] = 31;
 
         for (int i = 0; i < dias.length; i++) {
